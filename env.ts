6daf8ff0aee3419fda53d5f34d9da0d3d4e968d2
Dariusz Filipiak <darekf77@gmail.com>
2025-08-12 13:32:10 +0200

    first

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from "tnp/src";
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: "taon-containers.example.domain.com",
+    title: "Taon Containers",
+  },
+};
+export default env;
