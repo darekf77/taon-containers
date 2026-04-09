cff79ed9cff1eab84bd65370ad174fb2639e2f36
Dariusz Filipiak <darekf77@gmail.com>
2026-04-10 01:03:55 +0200

    fix: proper electron start

--- container-v21/isomorphic-lib-v21/src/app.electron.ts
+++ container-v21/isomorphic-lib-v21/src/app.electron.ts
@@ -70,18 +70,20 @@ function createWindow(): BrowserWindow {
 }
 
 async function startElectron() {
-  await start();
   try {
+    await app.whenReady();
+
+    await start();
+
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
     // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-    app.on('ready', () => setTimeout(createWindow, 400));
+    setTimeout(() => {
+      createWindow();
+    }, 400);
 
-    // Quit when all windows are closed.
     app.on('window-all-closed', () => {
-      // On OS X it is common for applications and their menu bar
-      // to stay active until the user quits explicitly with Cmd + Q
       if (process.platform !== 'darwin') {
         app.quit();
       }
@@ -94,9 +96,9 @@ async function startElectron() {
         createWindow();
       }
     });
-  } catch (e: unknown) {
-    // Catch Error
-    throw e;
+  } catch (err) {
+    console.error('startElectron error:', err);
+    throw err;
   }
 }
 
