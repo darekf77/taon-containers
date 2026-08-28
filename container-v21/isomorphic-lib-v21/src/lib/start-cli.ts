e92353ef0918f634d3abf8fba9931d7104f6c8a8
Dariusz Filipiak <darekf77@gmail.com>
2026-08-28 18:30:46 +0200

    feat: cli better start example

--- container-v21/isomorphic-lib-v21/src/lib/start-cli.ts
+++ container-v21/isomorphic-lib-v21/src/lib/start-cli.ts
@@ -1,13 +1,66 @@
 //#region tnp-helpers cli template
-// import { Helpers, BaseCommandLineFeature } from 'tnp-helpers/src';
-// import { BaseProject, BaseStartConfig } from 'tnp-helpers/src'; // @backend
+import { UtilsJson } from 'tnp-core/src';
+import { Helpers, BaseCommandLineFeature } from 'tnp-helpers/src';
+import { BaseProject, BaseStartConfig } from 'tnp-helpers/src'; // @backend
+//#endregion
 
-// class $Version extends BaseCommandLineFeature<{}> {
-//   public _() {
-//     console.log(`Hello world from cli`);
-//     this._exit();
-//   }
-// }
+//#region CLI / global scope
+/**
+ * This class is for handling global arguments. *
+ * Cli engine will parse: hello:world:from:global:arg to helloWorldFromGlobalArg
+ * (lower case, camel case) .. so you don't need to worry about misspelling
+ * arguments
+ */
+class $Global extends BaseCommandLineFeature<{}> {
+  /**
+   * When you execute cli without arguments:`$ cli`
+   */
+  public _() {
+    console.log(`Hello world from cli`);
+    this._exit();
+  }
+
+  /**
+   * When you execute: `$ cli hello:world:from:global:arg`
+   * (or `$ cli helloWorldFromGlobalArg`)
+   */
+  helloWorldFromGlobalArg() {
+    console.log('hello world from global argument!');
+    this._exit(0);
+  }
+}
+//#endregion
+
+//#region CLI / Version Scope
+/**
+ *
+ */
+class $Version extends BaseCommandLineFeature<{
+  anyCLIparamsHere: string;
+}> {
+  /**
+   * When you execute: `$ cli version`
+   */
+  public _() {
+    console.log(`Hello world from version argument`);
+    this._exit();
+  }
+
+  /**
+   * When you execute: `$ cli version:getFromPackageJson`
+   */
+  public getFromPackageJson() {
+    const ver = UtilsJson.getValue(
+      this.project.pathFor('package.json'),
+      'version',
+      {
+        defaultValue: '<not defined>',
+      },
+    );
+    console.log(`Version from packageJson ${ver}`);
+    this._exit(0);
+  }
+}
 //#endregion
 
 /**
@@ -15,30 +68,37 @@
  * @param argsv process.argsv
  * @param filename needed if you want ipc communicaiton
  */
-export async function startCli(argsv: string[], filename: string): Promise<void> {
+export async function startCli(
+  argsv: string[],
+  filename: string,
+): Promise<void> {
   //#region @backendFunc
   console.log('Hello from cli');
   console.log({ argsv });
-  process.exit(0);
+  process.exit(0); // comment this to use BaseStartConfig and class based cli
 
   //#region start config
-  // new BaseStartConfig({
-  //   ProjectClass: BaseProject,
-  //   functionsOrClasses: [
-  //     {
-  //       classOrFnName: '$Version',
-  //       funcOrClass: $Version,
-  //     },
-  //   ],
-  //   argsv,
-  //   useStringArrForArgsFunctions: true,
-  //   shortArgsReplaceConfig: {
-  //     v: 'version',
-  //   },
-  //   callbackNotRecognizedCommand: async () => {
-  //     Helpers.error(`Command not recognized`, false, true);
-  //   },
-  // });
+  new BaseStartConfig({
+    ProjectClass: BaseProject,
+    functionsOrClasses: [
+      {
+        classOrFnName: '', // registerd as global (only 1 class can be like this)
+        funcOrClass: $Global,
+      },
+      {
+        classOrFnName: '$Version',
+        funcOrClass: $Version,
+      },
+    ],
+    argsv,
+    useStringArrForArgsFunctions: true,
+    shortArgsReplaceConfig: {
+      v: 'version',
+    },
+    callbackNotRecognizedCommand: async () => {
+      Helpers.error(`Command not recognized`, false, true);
+    },
+  });
   //#endregion
   //#endregion
 }
